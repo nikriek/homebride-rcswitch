509baf709f95c9e4cd32f99c041bd144a0938c07
Niklas Riekenbrauck <nikriek@users.noreply.github.com>
2017-11-08 22:17:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,40 @@ module.exports = function(homebridge) {
 function RCSwitch(log, config) {
   this.log = log;
   this.config = config;
+  this.pin = config['pin'];
+  this.name = config['name'];
+  this.group = config['group'];
+  this.switch = config['switch'];
 }
+
+RCSwitch.prototype = {
+  getServices: function () {
+    let informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "RCSwitch")
+      .setCharacteristic(Characteristic.Model, "RCSwitch")
+      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+ 
+    let switchService = new Service.Switch(this.name);
+    switchService
+      .getCharacteristic(Characteristic.On)
+        .on('get', this.getSwitchOnCharacteristic.bind(this))
+        .on('set', this.setSwitchOnCharacteristic.bind(this));
+ 
+    this.informationService = informationService;
+    this.switchService = switchService;
+    return [informationService, switchService];
+  }
+};
+
+mySwitch.prototype = {
+ 
+  getSwitchOnCharacteristic: function (next) {
+    next(null, true);
+  },
+   
+  setSwitchOnCharacteristic: function (on, next) {
+    rcswitch.switchOn(this.group, this.switch)
+    next();
+  }
+};
