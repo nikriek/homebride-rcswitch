2ee7057bd8c834c8ce45f697171c779db1f76eba
Niklas Riekenbrauck <nikriek@users.noreply.github.com>
2017-11-08 22:09:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 var rcswitch = require('rcswitch');
+var Accessory, Service, Characteristic, UUIDGen;
+
+module.exports = function(homebridge) {
+  Accessory = homebridge.platformAccessory;
+
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  UUIDGen = homebridge.hap.uuid;
+  
+  homebridge.registerPlatform("homebridge-rcswitch", "RCSwitch", RCSwitch, true);
+}
+
+function RCSwitch(log, config) {
+  this.log = log;
+  this.config = config;
+}
