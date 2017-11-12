a595d7a1b77415518aeaaa314f1983badfdf6378
Niklas Riekenbrauck <nikriek@gmail.com>
2017-11-12 15:10:29 +0100

    Change

--- index.js
+++ index.js
@@ -23,34 +23,32 @@ function RCSwitch(log, config) {
   rcswitch.enableTransmit(this.pin);
 }
 
-RCSwitch.prototype.getServices = function () {
-    let informationService = new Service.AccessoryInformation();
-    informationService
-      .setCharacteristic(Characteristic.Manufacturer, "RCSwitch")
-      .setCharacteristic(Characteristic.Model, "RCSwitch")
-      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
-    
-    let switchService = new Service.Switch(this.name);
+RCSwitch.prototype.getServices = function() {
+  let informationService = new Service.AccessoryInformation();
+  informationService
+    .setCharacteristic(Characteristic.Manufacturer, "RCSwitch")
+    .setCharacteristic(Characteristic.Model, "RCSwitch")
+    .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+  
+  let switchService = new Service.Switch(this.name);
 
-    switchService.getCharacteristic(Characteristic.On)
-      .on('get', this.getSwitchOnCharacteristic.bind(this))
-      .on('set', this.setSwitchOnCharacteristic.bind(this))
+  switchService.getCharacteristic(Characteristic.On)
+    .on('get', this.getSwitchOnCharacteristic.bind(this))
+    .on('set', this.setSwitchOnCharacteristic.bind(this))
 
-    return [informationService, switchService];
+  return [informationService, switchService];
 }
 
-RCSwitch.prototype = {
-  getSwitchOnCharacteristic: function (next) {
-    next(null, this.isOn);
-  },
-   
-  setSwitchOnCharacteristic: function (on, next) {
-    if (on) {
-      rcswitch.switchOff(this.groupId, this.switchId);
-    } else {
-      rcswitch.switchOn(this.groupId, this.switchId);
-    }
-    this.isOn = !on;
-    next();
+RCSwitch.prototype.getSwitchOnCharacteristic = function(next) {
+  next(null, this.isOn);
+}
+
+RCSwitch.prototype.setSwitchOnCharacteristic = function(on, next) {
+  if (on) {
+    rcswitch.switchOff(this.groupId, this.switchId);
+  } else {
+    rcswitch.switchOn(this.groupId, this.switchId);
   }
+  this.isOn = !on;
+  next();
 }
