108592df0598c815dc1438ba92b6e90ae54c8114
Niklas Riekenbrauck <nikriek@gmail.com>
2017-11-12 14:38:01 +0100

    Change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function(homebridge) {
   Characteristic = homebridge.hap.Characteristic;
   UUIDGen = homebridge.hap.uuid;
   
-  homebridge.registerPlatform("homebridge-rcswitch", "RCSwitch", Platform, false);
+  homebridge.registerPlatform("homebridge-rcswitch", "RCSwitch", Platform, true);
 }
 
 function Platform(log, config, api) {
@@ -18,47 +18,47 @@ function Platform(log, config, api) {
   this.pin = config.pin['pin'] || 0;
   this.switches = config.switches || [];
   this.switchStates = Array(this.switches.length).fill(false);
-  this.accessories = [];
+  // this.accessories = [];
 }
 
 Platform.prototype.configureAccessory = function (accessory) {
   rcswitch.enableTransmit(this.pin);
-};
+}
 
-Platform.prototype.getServices = function () {
-    let informationService = new Service.AccessoryInformation();
-    informationService
-      .setCharacteristic(Characteristic.Manufacturer, "RCSwitch")
-      .setCharacteristic(Characteristic.Model, "RCSwitch")
-      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+// Platform.prototype.getServices = function () {
+//     let informationService = new Service.AccessoryInformation();
+//     informationService
+//       .setCharacteristic(Characteristic.Manufacturer, "RCSwitch")
+//       .setCharacteristic(Characteristic.Model, "RCSwitch")
+//       .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
     
-    let switchServices = this.switches.map(function(currentValue, index) { 
-        let switchService = new Service.Switch(currentValue.name);
-        return switchService.getCharacteristic(Characteristic.On)
-          .on('get', this.getSwitchOnCharacteristic.bind(this, index))
-          .on('set', this.setSwitchOnCharacteristic.bind(this, index));
-    });
+//     let switchServices = this.switches.map(function(currentValue, index) { 
+//         let switchService = new Service.Switch(currentValue.name);
+//         return switchService.getCharacteristic(Characteristic.On)
+//           .on('get', this.getSwitchOnCharacteristic.bind(this, index))
+//           .on('set', this.setSwitchOnCharacteristic.bind(this, index));
+//     });
        
-    this.informationService = informationService;
-    this.switchServices = switchServices;
-    return [informationService] + switchServices;
-};
+//     this.informationService = informationService;
+//     this.switchServices = switchServices;
+//     return [informationService] + switchServices;
+// };
 
-Platform.prototype = {
-  getSwitchOnCharacteristic: function (index, next) {
-    let isOn = this.switchStates[index];
-    next(null, isOn);
-  },
+// Platform.prototype = {
+//   getSwitchOnCharacteristic: function (index, next) {
+//     let isOn = this.switchStates[index];
+//     next(null, isOn);
+//   },
    
-  setSwitchOnCharacteristic: function (index, on, next) {
-    let isOn = this.switchStates[index];
-    let currentSwitch = this.switches[index];
-    if (isOn) {
-      rcswitch.switchOff(currentSwitch.group, currentSwitch.switch);
-    } else {
-      rcswitch.switchOn(currentSwitch.group, currentSwitch.switch);
-    }
-    this.switchStates[index] = !isOn;
-    next();
-  }
-};
+//   setSwitchOnCharacteristic: function (index, on, next) {
+//     let isOn = this.switchStates[index];
+//     let currentSwitch = this.switches[index];
+//     if (isOn) {
+//       rcswitch.switchOff(currentSwitch.group, currentSwitch.switch);
+//     } else {
+//       rcswitch.switchOn(currentSwitch.group, currentSwitch.switch);
+//     }
+//     this.switchStates[index] = !isOn;
+//     next();
+//   }
+// };
